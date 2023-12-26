e1d8ade5e7bd25e2b270e3a14499aa3e236dc12b
Mrwan Zein <mrwan@wecre8websites.com>
2023-12-26 11:29:41 -0500

    changed font & updated tutorial

--- src/components/tutorial/Tutorial.jsx
+++ src/components/tutorial/Tutorial.jsx
@@ -7,7 +7,7 @@ const Tutorial = () => {
             <div>
                 <h2>Goal of the game</h2>
                 <p>
-                    The goal of the game is simple, you have to bring the opponent's health to 0.
+                    The goal of the game is simple, bring the opponent's health to 0.
                 </p>
             </div>
 
@@ -15,31 +15,31 @@ const Tutorial = () => {
                 <h2>How to play</h2>
                 
                 <p>
-                    As a word game in the style of <a href="https://en.wikipedia.org/wiki/Shiritori">shiritori</a> (a japanese word game), you have to
-                    attack your opponent by writing words in the inputs. The tricky part is to defend an attacking word. You have to use the
+                    As a word game in the style of <a href="https://en.wikipedia.org/wiki/Shiritori">shiritori</a> (a japanese word game), the main action is to
+                    attack your opponent by writing words in the inputs. The tricky part is the defending. The player has to use the
                     last letter of the opponent's word and the length of the word must be equal or higher than the attacking one.
                 </p>
 
-                <p>Let's look at the image below. Under that image, you will find what the numbers mean.</p>
+                <p>Let's look at the image below. Under that image, the numbers will be explained.</p>
                 
                 <TutorialImg1 src="/src/assets/images/tutorialImg1.jpg" alt="image with numbers explaining how to play" />
 
                 <ol>
                     <TutorialListInstance>
-                        The input field where you have to write the words you want to attack with (press the enter key after writing a word).
-                        It can be use for attacking or defending. If there's no arrow pointing to that input, it means you can attack.
-                        If there is an arrow, it means you're getting attacked and you are now in a duel for that specific input. The players have
+                        The input field where the words are written (press the enter key after writing a word).
+                        It can be used for attacking or defending. If there's no arrow pointing to that input, it means it can attack.
+                        If there is an arrow, it means its getting attacked and a duel is in motion for that specific input. The players have
                         to go back and forth until the timer on a duel arrow expires.
                     </TutorialListInstance>
 
                     <TutorialListInstance>
-                        The selected target (press the input with the mouse to select a target). You can attack multiple inputs at the same time, but not the ones that
-                        are already active. You have to be quick and accurate because it can rapidly become overwhelming.
+                        The selected target to attack (press the opponent's input with the mouse to select). It is possible to attack multiple inputs at the same time
+                        but not the ones that are already active. It is recommended to be quick and accurate because it can rapidly become overwhelming.
                     </TutorialListInstance>
 
                     <TutorialListInstance>
-                        The timing arrow. After attacking or defending with a word, you can see an arrow with a number attached to it. That number is
-                        the remaining seconds you have to defend. Once it expires, you take the length of the word as damage to your health.
+                        The timing arrow. After attacking or defending a word, there's an arrow with a number attached to it. That number are
+                        the remaining seconds left to defend. Once it expires, the defendant takes the length of the word as damage to the health bar.
                     </TutorialListInstance>
 
                     <TutorialListInstance>
@@ -68,8 +68,7 @@ const Wrapper = styled.div`
     overflow-y: scroll;
     flex: 1;
     padding: 15px;
-    line-height: 1.5;
-
+    line-height: 1.75;
 `
 
 const TutorialTitle = styled.span`
