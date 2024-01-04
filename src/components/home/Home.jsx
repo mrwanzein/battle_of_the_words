9f35dc416e2c5f3d33e05ef3688a00a6f1fabb75
Mrwan Zein <mrwan@wecre8websites.com>
2024-01-04 12:45:08 -0500

    added 1st version of homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,38 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
     return (
-        <h1>welcome home</h1>
+        <Wrapper>
+            <p>
+                Hi, I'm Mrwan Zein and this is battle of the words. It's a free word game that I created after being inspired by <a href="https://shiritorigame.com/" target="_blank" rel="noopener noreferrer">Shiritori game</a>. 
+                I found that version of the game to be too unidimensional, so I added my own sauce to it by making it more dynamic. The game doesn't support playing against
+                the computer for now, but it will be a feature in the future.
+            </p>
+
+            <p>
+                Press <NavLink to="/tutorial">here</NavLink> to understand how to play the game.
+            </p>
+
+            <p>
+                <NavLink to="/rooms">Create a room</NavLink> and tell a friend to start playing!
+            </p>
+
+            <p>
+                The dictionaries are based on the word lists from this <a href="https://github.com/lorenbrichter/Words/tree/master/Words" target="_blank" rel="noopener noreferrer">github repository </a>
+            </p>
+
+            <p>
+                To contact me, it can be by email at mrwanzein@outlook.com or through <a href="https://www.linkedin.com/in/mrwanzein" target="_blank" rel="noopener noreferrer">linkedin</a>.
+            </p>
+        </Wrapper>
     )
 }
 
 export default Home;
+
+const Wrapper = styled.div`
+    margin: 20px;
+    padding: 15px;
+    line-height: 1.75;
+`
